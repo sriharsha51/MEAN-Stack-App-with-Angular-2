167b4795baa59d3c0ad3c95ccc54b2859a477c50
sriharsha51 <sriharsha.alu@gmail.com>
2017-10-12 20:02:04 -0400

    deployment

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ app.use(cors({
 }));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/client/dist/'));
+app.use(express.static(__dirname + '/public'));
 app.use('/authentication', authentication);
 app.use('/blogs', blogs);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+  res.sendFile(path.join(__dirname + 'public/index.html'));
 });//connect server to index page of angular
 
 app.listen(port, () => {
